d0c2e632e59904ca43e89ca48901282750ba53a0
Matheus Servulo Cajé <camilevitoriarosa2005@gmail.com>
2022-09-25 13:27:38 -0300

    foi

--- site/src/API/CadProduto.js
+++ site/src/API/CadProduto.js
@@ -1,27 +1,27 @@
 import { API_URL } from './config';
 
 import axios from 'axios'
-import { con } from '../../../api/src/repository/connection';
+//import { con } from '../../../api/src/repository/connection';
 const api = axios.create({
     baseURL: API_URL
 })
 
 export async function CadastrarPoduto(id, nome, descricao, preco, disponivel, usuario) {
     const resposta= await api.post('/produto',{
-        id,
-        nome, 
-        preco, 
-        descricao,
-        disponivel,
-        usuario
+        id: id,
+        nome: nome, 
+        preco: preco, 
+        descricao:descricao,
+        disponivel: disponivel,
+        usuario: usuario
     } )
     return resposta.data
 }
 
 export async function CadastrarCor(id,cor) {
     const resposta= await api.post ('/cor', {
-        id,
-        cor
+        id: id,
+        cor: cor
     })
     return resposta.data
 }
@@ -29,8 +29,20 @@ export async function CadastrarCor(id,cor) {
 
 export async function CadastrarTamanho(id, tamanho) {
     const resposta= await api.post ('/tamanho', {
-        id,
-        tamanho
+        id: id,
+        tamanho: tamanho
     })
+
+    export async function listarCategorias() {
+        const resp = await api.get('/api/categoria')
+        return resp.data;
+    }
+
+    export async function listarTemas() {
+        const resp = await api.get('/api/tema')
+        return resp.data;
+    }
+    
+    
     return resposta.data
 }
--- site/src/pages/Administrador/AddProduto/index.js
+++ site/src/pages/Administrador/AddProduto/index.js
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 import MenuAdmin from "../../../components/pagAdm.js";
 import "./index.scss";
 import "../../../common/common.scss"
+import { useState } from "react";
 
 export default function InserirProduto() {
+    const [id, setId]= useState('');
+    const [nome, setNome] = useState('');
+    const [desc, setDesc] = useState('');
+    const [valor, setValor] = useState('');
+    const [disponivel, setDisponivel] = useState(false);
     return (
         <main className="inserirProduto">
 
@@ -19,17 +25,17 @@ export default function InserirProduto() {
                         <div className="div1">
                             <div className="flex-column">
                                 <label> Código do Produto </label>
-                                <input className="input"/>
+                                <input type="text" value={id} onChange={e=> setId(e.target.value)} className="input"/>
                             </div>
 
                             <div className="flex-column">
                                 <label> Nome do Produto </label>
-                                <input className="input"/>
+                                <input type="text" value={nome} onChange={e=> setNome(e.target.value)} className="input"/>
                             </div>
 
                             <div className="flex-column">
                                 <label> Descrição do Produto </label>
-                                <textarea maxLength={100} className="textarea"/>
+                                <textarea type="text" value={desc} onChange={e=> setDesc(e.target.value)} maxLength={100} className="textarea"/>
                             </div>
 
                             
@@ -68,13 +74,13 @@ export default function InserirProduto() {
 
                             <div className="flex-column"> 
                                 <label > Valor </label>
-                                <input className="input"/>
+                                <input type="text" value={valor} onChange={e=> setValor(e.target.value)} className="input"/>
                             </div>
 
                             <div>
                                 <label> Disponível ? </label>
-                                    <div> <label> Sim </label> <input type='checkbox'/> </div>
-                                    <div> <label> Não </label> <input type='checkbox'/> </div>
+                                    <div> <label type="checked" value={disponivel} onChange={e=> setDisponivel(e.target.checked)}> Sim </label> <input type='checkbox'/> </div>
+                                    <div> <label type="checked" value={disponivel} onChange={e=> setDisponivel(e.target.checked)}> Não </label> <input type='checkbox'/> </div>
                             </div> 
                         </div>
 
