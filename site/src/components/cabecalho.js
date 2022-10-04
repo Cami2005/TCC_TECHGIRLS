e61e3d5421bee31dc12b4a5be07577e76bc4fd64
Matheus Servulo Cajé <camilevitoriarosa2005@gmail.com>
2022-10-04 01:25:44 -0300

    indo

--- site/src/components/cabecalho.js
+++ site/src/components/cabecalho.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+import { buscarProdutosPorNome } from '../API/CadProduto.js';
 import './cabecalho.scss';
 
 export default function Cabecalho(props){
+
+    const [produtos, setProdutos] = useState([]);
+
+    const [busca, setBusca] = useState('');
+
+    async function buscarNomeClick() {
+        const resp = await buscarProdutosPorNome(busca);
+        console.log(resp)
+        setProdutos(resp);
+     }
+
     return(
             <header className="cab">
                 <img className="logo" src={props.logo}/>
                 <img  className='menuCab' src={props.menu}/>
                 <div>
-                <input className='busca' placeholder={props.placeholder} type={props.type} onChange={props.onChange} ></input>
-                <button><img className='icons' src={props.lupa}></img></button>
+                <input className='busca' placeholder='encontre seu produto aqui' type='text' value={busca} onChange={e=> setBusca(e.target.value)} />
+                <button onClick={buscarNomeClick}>pesquisar</button>
 
                 </div>
                 <img className='icons' src={props.fav}></img>
--- site/src/pages/Usuario/nossosProdutos/index.js
+++ site/src/pages/Usuario/nossosProdutos/index.js
@@ -1,12 +1,37 @@
 import './index.scss'
 
 import Cabecalho from "../../../components/cabecalho.js";
+import { useEffect, useState } from 'react';
+import { ProdutosListados } from '../../../API/CadProduto';
 
 export default function Index(){
+    const[produto, setProduto] = useState([]);
+
+
+    async function ListarProdutos(){
+        const resposta= await ProdutosListados();
+        setProduto(resposta)
+    }
+
+    
+        useEffect(() => {
+            ListarProdutos();
+        } , [])
     
     return(
         <main>
-            <Cabecalho logo='../../../images/logoAdmin.png' menu='../../../images/menu.png' lupa='../../../images/lupa.png' fav='../../../images/favoritos.png' user='../../../images/user.png' sacola='../../../images/sacola.png'/>
+            <Cabecalho logo='../../../images/logoAdmin.png' menu='../../../images/menu.png'  fav='../../../images/favoritos.png' user='../../../images/user.png' sacola='../../../images/sacola.png'/>
+        
+            <div>
+                    {produto.map(item =>
+                    <div>
+                        <p>{item.nome}</p>
+                        
+                    </div>
+                        
+                     )}
+                </div>
+        
         </main>
     )
 }
