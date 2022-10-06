6b4bf1824277ce8352a27b1f26ac21416bfbc947
Matheus Servulo Cajé <camilevitoriarosa2005@gmail.com>
2022-10-05 23:35:10 -0300

    subindo endpoints de endereço

--- api/src/controller/usuarioController.js
+++ api/src/controller/usuarioController.js
@@ -1,5 +1,5 @@
 import { Router } from "express"; 
-import { inserirUsuario } from "../repository/usuarioRepository.js";
+import { alterarEndereco, deletarEndereco, inserirEndereco, inserirNovoCartao, inserirUsuario, listarEnderecos } from "../repository/usuarioRepository.js";
 
 const server = Router(); 
 
@@ -31,5 +31,119 @@ server.post('/usuario', async (req, resp) => {
    
 })
 
+server.post('/endereco', async (req, resp) => {
+            try {
+              
+                const endereco= req.body;
+                
+                const novoEndereco= await inserirEndereco(endereco);
+                if(!novoEndereco.cep) {
+                    throw new Error('Id não registrado');
+                    };
+                    if(!novoEndereco.residencia) {
+                        throw new Error('Nome não registrado');
+                    };
+                    if(!novoEndereco.endereco) {
+                        throw new Error('Tema não registrado');
+                    };
+                    if(!novoEndereco.bairro) {
+                        throw new Error('Categoria não registrada');
+                    };
+                    if(!novoEndereco.estado) {
+                        throw new Error('Preço não registrado')
+                    }
+                    if(!novoEndereco.uf) {
+                            throw new Error('Descrição não registrada')
+                    }
+                    if(!novoEndereco.complemento) {
+                        throw new Error('Descrição não registrada')
+                }
 
+            
+                    resp.send(novoEndereco);
+
+            } catch (err) {
+                resp.status(404).send({
+                    erro: err.message
+                })
+            }
+
+})
+
+server.get('/endereco', async (req, resp) => {
+    try {
+        const resposta = await listarEnderecos();
+        resp.send(resposta);
+    
+    } catch (err) {
+            resp.status(404).send ({
+                erro: err.message
+            })    
+    }
+})
+
+server.put('/endereco/:id', async (req, resp) => {
+    try {
+        const {id} = req.params;
+        const endereco = req.body;
+
+        const resposta = await alterarEndereco(id, endereco);
+
+        resp.send(resposta);
+
+    } catch (err) {
+        resp.status(404).send({
+            err: err.message
+        })
+    }
+})
+
+server.delete('/endereco/:id', async (req,resp) => {
+    	try {
+    		const {id} = req.params;
+    
+    		const resposta = await deletarEndereco(id);
+    		if(resposta != 1)
+    			throw new Error('Endereço não pode ser removido');
+    			resp.status(204).send();
+    		}
+    
+    		catch (err) {
+    		resp.status(400).send ({
+    		  erro: err.message
+    			})
+    		   }
+    		})
+
+            server.post('/cartao', async (req, resp) => {
+                try {
+                  
+                    const cartao= req.body;
+                    
+                    const novoCartao= await inserirNovoCartao(cartao);
+                    if(!novoCartao.numero) {
+                        throw new Error('Id não registrado');
+                        };
+                        if(!novoCartao.validade) {
+                            throw new Error('Nome não registrado');
+                        };
+                        if(!novoCartao.codigo) {
+                            throw new Error('Tema não registrado');
+                        };
+                        if(!novoCartao.cpf) {
+                            throw new Error('Categoria não registrada');
+                        };
+
+    
+                
+                        resp.send(novoCartao);
+    
+                } catch (err) {
+                    resp.status(404).send({
+                        erro: err.message
+                    })
+                }
+    
+    })
+    
 export default server;
