c2aafc486306f344998476201fd90c24569aad9a
Matheus Servulo Cajé <camilevitoriarosa2005@gmail.com>
2022-10-17 15:15:51 -0300

    ggg

--- site/src/pages/Usuario/Favoritos/index.js
+++ site/src/pages/Usuario/Favoritos/index.js
@@ -8,7 +8,7 @@ export default function Index(){
         <main>
             <CabecalhoPrincipal  logo='../../../images/logoAdmin.png' menu='../../../images/menu.png'  fav='../../../images/favoritos.png' 
             user='../../../images/user.png' sacola='../../../images/sacola.png'/>
-            <Tema className='faixa' nome='iiii'/>
+            <Tema nome='iiii'/>
         </main>
     )
 }
